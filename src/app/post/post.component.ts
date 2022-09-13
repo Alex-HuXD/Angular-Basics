b821e34a6631be78959540be366b3bab3dc94f2f
XinDi Hu <huxdev@pm.me>
2022-09-13 15:58:09 -0500

    generate component & data flow betweem parent&child components

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
-export class PostComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PostComponent {
+  @Input() postImg = '';
 }

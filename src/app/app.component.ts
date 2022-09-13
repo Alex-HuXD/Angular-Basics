185695060743bd11e88818d2946a40b3c97efe5f
XinDi Hu <huxdev@pm.me>
2022-09-12 22:57:09 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // styles: ['h2 {color:red;}'],
+})
+export class AppComponent {}

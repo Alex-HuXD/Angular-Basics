59b314a27abeb27dbe1a807c0fdafe2c9321bb56
XinDi Hu <huxdev@pm.me>
2022-09-15 22:24:02 -0500

    lifecycle methods and css scope

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,34 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css'],
 })
-export class PostComponent {
+export class PostComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input() postImg = '';
 
   @Output() clickImg = new EventEmitter<{ name: string; payload: string }>();
@@ -17,4 +40,52 @@ export class PostComponent {
     };
     this.clickImg.emit(data);
   };
+
+  //run first & run once
+  // - creates component with default value set inside this class
+  constructor() {
+    console.log('constructor() called', this.postImg);
+  }
+
+  // if @input() exist run 3rd after ngOnChanges()
+  // other wise run 2nd
+  //run once,
+  // - component will receives @input data after constructor() and init the component
+  ngOnInit() {
+    console.log('ngOnInit() called', this.postImg);
+  }
+
+  //run 2nd if @input() exist,
+  // other wise after ngOnInit() whenever change detection found state changes
+  ngOnChanges(): void {
+    console.log('ngOnChanges() called');
+  }
+
+  //eun everytime something happens, even the content doesn't change.
+  ngDoCheck(): void {
+    console.log('ngDoChenk() called');
+  }
+
+  //for the following lifecycle, need to understand view VS content
+  // View refers to the template of the component itself
+  // content refers to the projectd content from parent component
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit() called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentCkecked() called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit() called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked() called');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy() called');
+  }
 }
